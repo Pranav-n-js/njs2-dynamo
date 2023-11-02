2fab90e7bb0a2b456093b200b65a3227255c26de
Pranav-n-js <=>
2023-11-02 14:08:26 +0530

    Updated Index for schema

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const createModel = require('./interface/createModel');
 const createSchema = require('./interface/createSchema');
-
+const Schema = require('./interface/Schema');
 class DYNAMOManager {
   /**
    * Database Create Schema
@@ -8,7 +8,7 @@ class DYNAMOManager {
    * @param {Object} collectionStructure
    * @returns {Promise<Number>}
    */
-  static createSchema = createSchema();
+  static createSchema = createSchema;
 
   /**
    * Database Create Model
@@ -18,7 +18,9 @@ class DYNAMOManager {
    * @param {string} collectionNameInDb
    * @returns {Promise<Object>}
    */
-  static createModel = createModel();
+  static createModel = createModel;
+
+  static createSchema = Schema;
 }
 
 module.exports = DYNAMOManager;
