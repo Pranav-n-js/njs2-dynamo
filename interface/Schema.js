e36511bcdec332b93a9dadd41ec54907fca9552d
Pranav-n-js <=>
2023-11-15 11:57:27 +0530

    Feat: changed connection settings

--- interface/Schema.js
+++ interface/Schema.js
@@ -54,27 +54,6 @@ class Schema {
 
     connection() {
 
-        if (!process.env['@njs2/dynamo']) {
-            throw new Error('REQUIRED @njs2-dynamo in environment variable');
-        }
-
-        let envVariables = typeof process.env['@njs2/dynamo'] == 'object' ? process.env['@njs2/dynamo'] : JSON.parse(process.env['@njs2/dynamo']);
-
-        if (!envVariables.USE_IAM_ROLE || envVariables.LOCAL_DB) {
-
-            if (!envVariables.AWS_REGION) {
-                throw new Error("AWS_REGION must be defined");
-            }
-
-            if (!envVariables.AWS_ACCESS_KEY_ID) {
-                throw new Error("AWS_ACCESS_KEY_ID must be defined");
-            }
-
-            if (!envVariables.AWS_SECRET_ACCESS_KEY) {
-                throw new Error("AWS_SECRET_ACCESS_KEY must be defined");
-            }
-        }
-
         AWS.config.update({
             region: this.region
         });
